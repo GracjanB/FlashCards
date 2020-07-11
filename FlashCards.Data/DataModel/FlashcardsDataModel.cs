6aa7262dd6c12eb307d2b1f5ab27991455295b1d
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-07-11 14:31:50 +0200

    Added DbContext for database with needed packages

---
+++ FlashCards.Data/DataModel/FlashcardsDataModel.cs
@@ -0,0 +1,36 @@
+﻿using FlashCards.Data.Models;
+using FlashCards.Data.ModelsConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlashCards.Data.DataModel
+{
+    public class FlashcardsDataModel : DbContext
+    {
+        public FlashcardsDataModel(DbContextOptions<FlashcardsDataModel> options) : base(options) { }
+
+        public DbSet<Account> Accounts { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<CourseInfo> CourseInfos { get; set; }
+
+        public DbSet<CourseOpinion> CourseOpinions { get; set; }
+
+        public DbSet<Lesson> Lessons { get; set; }
+
+        public DbSet<Flashcard> Flashcards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AccountConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new CourseConfiguration());
+            modelBuilder.ApplyConfiguration(new CourseInfoConfiguration());
+            modelBuilder.ApplyConfiguration(new CourseOpinionConfiguration());
+            modelBuilder.ApplyConfiguration(new LessonConfiguration());
+            modelBuilder.ApplyConfiguration(new FlashcardConfiguration());
+        }
+    }
+}
